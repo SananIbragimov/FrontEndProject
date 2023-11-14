50803b805a0e50975b4cb63ddff602b945091e19
SananIbragimov <sibrahimov925@gmail.com>
2023-11-14 17:09:41 +0400

    Top header was completed

---
+++ assets/scripts/js/main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("languageDropdown").addEventListener("click", function () {
+    this.classList.toggle("up");
+});
+document.getElementById("currencyDropdown").addEventListener("click", function () {
+    this.classList.toggle("up");
+});
