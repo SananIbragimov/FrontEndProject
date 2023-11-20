637a43958c153b47a1caa6107259995996f1bbec
SananIbragimov <sibrahimov925@gmail.com>
2023-11-20 13:10:14 +0400

    Navbar dropdown menu added to site

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -8,6 +8,24 @@
     });
 }
 {
+    $(document).ready(function () {
+        $('.header__end__btn').click(function () {
+            var rotation = $(this).hasClass('rotate180') ? 'rotate(0deg)' : 'rotate(180deg)';
+            $(this).toggleClass('rotate180');
+            $(this).find('i').css('transform', 'rotateY(180deg)'); // Sağdan dönme
+        });
+    
+        // Menü açıkken herhangi bir yere tıklandığında simge durumunu sıfırla
+        $(document).on('click', function (e) {
+            if (!$('.header__end__dropdown').is(e.target) && $('.header__end__dropdown').has(e.target).length === 0) {
+                $('.header__end__btn').removeClass('rotate180');
+                $('.header__end__btn i').css('transform', 'rotateY(0deg)'); // Sağdan dönme sıfırlama
+            }
+        });
+    });
+    
+}
+{
     document.addEventListener('DOMContentLoaded', function () {
         // Set the date when the discount ends
         var discountEndDate = new Date("Nov 30, 2023 00:00:00").getTime();
@@ -40,4 +58,100 @@
         }, 1000);
     });
 
+}
+
+// Navbar modals
+{
+    $(document).ready(function () {
+        $('.navbar-nav .dropdown').on('show.bs.dropdown', function () {
+            $(this).find('.dropdown-toggle').addClass('rotate');
+        }).on('hide.bs.dropdown', function () {
+            $(this).find('.dropdown-toggle').removeClass('rotate');
+        });
+    
+        $("#person-icon").click(function () {
+            // Show your modal here
+            $("#loginModal").modal("show");
+        });
+    });
+    
+    $(document).ready(function () {
+        // Initialize the modal
+        $(document).ready(function () {
+            // Click event for the person icon
+            $("#person-icon").on("click", function () {
+                // Show the modal
+                $('#loginRegisterModal').modal('show');
+            });
+    
+            // Click event for the close button inside the modal
+            $(".close").on("click", function () {
+                // Close the modal
+                $('#loginRegisterModal').modal('hide');
+            });
+        });
+        
+        
+    });
+    
+    function openTab(evt, tabName) {
+        // Hide all tab contents
+        $(".tabcontent").hide();
+        
+        // Deactivate all tab links
+        $(".tablink").removeClass("active");
+        
+        // Show the selected tab content
+        $("#" + tabName).show();
+        
+        // Activate the clicked tab link
+        $(evt.target).addClass("active");
+    }
+    
+    // Click event for the close button inside the modal
+    $(".close").on("click", function () {
+        // Close the modal
+        $('#loginRegisterModal').modal('hide');
+    });
+    
+    function openTab(evt, tabName) {
+        // Hide all tab contents
+        $(".tabcontent").hide();
+        
+        // Deactivate all tab links
+        $(".tablink").removeClass("active").css("color", ""); // Reset text color
+        
+        // Show the selected tab content
+        $("#" + tabName).show();
+        
+        // Activate the clicked tab link and change its text color to goldenrod
+        $(evt.target).addClass("active").css("color", "goldenrod");
+    }
+    
+    $(document).ready(function () {
+        // close-btn üzerine gelindiğinde animasyon ekleyin
+        $(".close").hover(
+            function () {
+                $(this).css("transform", "rotate(180deg)");
+            },
+            function () {
+                $(this).css("transform", "rotate(0deg)");
+            }
+        );
+    });
+    
+    // Right sidebar
+    $(document).ready(function () {
+        // Click event for the fa-bars-staggered icon
+        $("#openSidebar").on("click", function () {
+            // Execute code before the modal is shown
+            $('#sidebarRight').on('show.bs.modal', function () {
+                // Your code to execute before the modal is shown
+            });
+    
+            // Show the right sidebar modal
+            $('#sidebarRight').modal('show');
+        });
+    });
+    
 }
