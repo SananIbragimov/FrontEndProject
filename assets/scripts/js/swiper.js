519380531aee82f7a24db653d1f07a4fe8f5b1b7
SananIbragimov <sibrahimov925@gmail.com>
2023-11-22 16:38:18 +0400

    About page added to project

--- assets/scripts/js/swiper.js
+++ assets/scripts/js/swiper.js
@@ -1,6 +1,7 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
-// Initialize Swiper
+{
+  // Initialize Swiper
 const swiper = new Swiper('.swiper', {
   // Optional: Add your configuration options here
   slidesPerView: 1,
@@ -23,3 +24,44 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+}
+
+
+{
+  const swiper01 = new Swiper('#swiper-brand', {
+    loop: true,
+    slidesPerView: 5,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 5,
+      },
+    },
+    autoplay: false,
+    mousewheel: false, // Mouse tekerleği kontrolünü devre dışı bırak
+    grabCursor: true,
+  });
+  
+  
+}
+
+
+
+
+
+
+
+
+
