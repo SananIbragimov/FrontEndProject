8563354d5aaa670d6b5d5bd8cd60a12c77471915
SananIbragimov <sibrahimov925@gmail.com>
2023-11-26 13:41:34 +0400

    Add to cart and its responsive features added to project

--- assets/scripts/js/main.js
+++ assets/scripts/js/main.js
@@ -1,176 +1,47 @@
-
 {
-    document.getElementById("languageDropdown").addEventListener("click", function () {
-        this.classList.toggle("up");
+  $(document).ready(function () {
+    $('.header__end__btn').click(function () {
+      var rotation = $(this).hasClass('rotate180') ? 'rotate(0deg)' : 'rotate(180deg)';
+      $(this).toggleClass('rotate180');
+      $(this).find('i').css('transform', 'rotateY(180deg)');
     });
-    document.getElementById("currencyDropdown").addEventListener("click", function () {
-        this.classList.toggle("up");
-    });
-}
-{
-    $(document).ready(function () {
-        $('.header__end__btn').click(function () {
-            var rotation = $(this).hasClass('rotate180') ? 'rotate(0deg)' : 'rotate(180deg)';
-            $(this).toggleClass('rotate180');
-            $(this).find('i').css('transform', 'rotateY(180deg)');
-        });
-    
-        
-        $(document).on('click', function (e) {
-            if (!$('.header__end__dropdown').is(e.target) && $('.header__end__dropdown').has(e.target).length === 0) {
-                $('.header__end__btn').removeClass('rotate180');
-                $('.header__end__btn i').css('transform', 'rotateY(0deg)');
-            }
-        });
-    });
-    
-}
-{
-    document.addEventListener('DOMContentLoaded', function () {
-        // Set the date when the discount ends
-        var discountEndDate = new Date("Nov 30, 2023 00:00:00").getTime();
+  });
 
-        // Update the countdown every 1 second
-        var x = setInterval(function () {
-            // Get the current date and time
-            var now = new Date().getTime();
-
-            // Calculate the remaining time
-            var distance = discountEndDate - now;
-
-            // Calculate days, hours, minutes, and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            // Display the countdown in the respective elements
-            document.getElementById("days").innerHTML = days;
-            document.getElementById("hours").innerHTML = hours;
-            document.getElementById("minutes").innerHTML = minutes;
-            document.getElementById("seconds").innerHTML = seconds;
-
-            // If the countdown is over, display a message
-            if (distance < 0) {
-                clearInterval(x);
-                document.getElementById("countdown").innerHTML = "Discount Expired";
-            }
-        }, 1000);
-    });
-
-}
-
-// Navbar modals
-{
-    $(document).ready(function () {
-        $('.navbar-nav .dropdown').on('show.bs.dropdown', function () {
-            $(this).find('.dropdown-toggle').addClass('rotate');
-        }).on('hide.bs.dropdown', function () {
-            $(this).find('.dropdown-toggle').removeClass('rotate');
-        });
-    
-        $("#person-icon").click(function () {
-            // Show your modal here
-            $("#loginModal").modal("show");
-        });
-    });
-    
-    $(document).ready(function () {
-        // Initialize the modal
-        $(document).ready(function () {
-            // Click event for the person icon
-            $("#person-icon").on("click", function () {
-                // Show the modal
-                $('#loginRegisterModal').modal('show');
-            });
-    
-            // Click event for the close button inside the modal
-            $(".close").on("click", function () {
-                // Close the modal
-                $('#loginRegisterModal').modal('hide');
-            });
-        });
-        
-        
-    });
-    
-    function openTab(evt, tabName) {
-        // Hide all tab contents
-        $(".tabcontent").hide();
-        
-        // Deactivate all tab links
-        $(".tablink").removeClass("active");
-        
-        // Show the selected tab content
-        $("#" + tabName).show();
-        
-        // Activate the clicked tab link
-        $(evt.target).addClass("active");
+  $(document).on('click', function (e) {
+    if (!$('.header__end__dropdown').is(e.target) && $('.header__end__dropdown').has(e.target).length === 0) {
+      $('.header__end__btn').removeClass('rotate180');
+      $('.header__end__btn i').css('transform', 'rotateY(0deg)');
     }
-    
-    // Click event for the close button inside the modal
-    $(".close").on("click", function () {
-        // Close the modal
-        $('#loginRegisterModal').modal('hide');
-    });
-    
-    function openTab(evt, tabName) {
-        // Hide all tab contents
-        $(".tabcontent").hide();
-        
-        // Deactivate all tab links
-        $(".tablink").removeClass("active").css("color", ""); // Reset text color
-        
-        // Show the selected tab content
-        $("#" + tabName).show();
-        
-        // Activate the clicked tab link and change its text color to goldenrod
-        $(evt.target).addClass("active").css("color", "goldenrod");
-    }
-    
-    $(document).ready(function () {
-        // close-btn üzerine gelindiğinde animasyon ekleyin
-        $(".close").hover(
-            function () {
-                $(this).css("transform", "rotate(180deg)");
-            },
-            function () {
-                $(this).css("transform", "rotate(0deg)");
-            }
-        );
-    });
-    
-    // Right sidebar
-    $(document).ready(function () {
-        // Click event for the fa-bars-staggered icon
-        $("#openSidebar").on("click", function () {
-            // Execute code before the modal is shown
-            $('#sidebarRight').on('show.bs.modal', function () {
-                // Your code to execute before the modal is shown
-            });
-    
-            // Show the right sidebar modal
-            $('#sidebarRight').modal('show');
-        });
-    });
-    
+  });
+
 }
 
-// Accordion
 {
-    function toggleAccordion(header) {
-        var content = header.nextElementSibling;
-    
-        if (content.style.maxHeight && content.style.maxHeight !== "0px") {
-            content.style.maxHeight = "0";
-            header.classList.remove('active');
-        } else {
-            content.style.maxHeight = "500px";
-            header.classList.add('active');
-        }
-    }
-    
-    
-    
-    
+  document.addEventListener('DOMContentLoaded', function () {
+    const openModalBtn = document.getElementById('openModalBtn');
+    const closeModalBtn = document.getElementById('closeModalBtn');
+    const modal = document.getElementById('myModal');
+
+    openModalBtn.addEventListener('click', function () {
+      modal.style.display = 'block';
+    });
+
+    closeModalBtn.addEventListener('click', function () {
+      modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
+
+  document.querySelector('.modal__product__section__cart').addEventListener('mouseover', function () {
+    this.style.cursor = 'pointer';
+  });
+
+  document.querySelector('.modal__product__section__cart').addEventListener('mouseout', function () {
+    this.style.cursor = 'auto';
+  });
 }
--- assets/scripts/js/swiper.js
+++ assets/scripts/js/swiper.js
@@ -1,63 +1,81 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
 {
   // Initialize Swiper
-const swiper = new Swiper('.swiper', {
-  // Optional: Add your configuration options here
-  slidesPerView: 1,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-  // Autoplay configuration
-  autoplay: {
-    delay: 5000, // Set the delay between slides in milliseconds (5 seconds in this case)
-    disableOnInteraction: false, // Allow manual interaction to interrupt autoplay
-  },
-
-});
-}
-
-
-{
-  const swiper01 = new Swiper('#swiper-brand', {
-    loop: true,
-    slidesPerView: 5,
+  const swiper = new Swiper('.swiper', {
+    // Optional: Add your configuration options here
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
-      dynamicBullets: true,
     },
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      1200: {
-        slidesPerView: 5,
-      },
+    scrollbar: {
+      el: '.swiper-scrollbar',
     },
-    autoplay: false,
-    mousewheel: false, // Mouse tekerleği kontrolünü devre dışı bırak
-    grabCursor: true,
+    // Autoplay configuration
+    autoplay: {
+      delay: 5000, // Set the delay between slides in milliseconds (5 seconds in this case)
+      disableOnInteraction: false, // Allow manual interaction to interrupt autoplay
+    },
+
   });
-  
-  
 }
 
+{
+  const swiper2 = new Swiper('#swiper-brand', {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
 
+  let isMouseDown = false;
+
+  swiper2.on('mousedown', function () {
+    isMouseDown = true;
+    swiper2.autoplay.stop(); // Stopping autoplay
+  });
+
+  swiper2.on('mouseup', function () {
+    isMouseDown = false;
+    swiper2.autoplay.start(); // Starting autoplay
+  });
+
+  swiper2.on('transitionStart', function () {
+    if (isMouseDown) {
+      swiper2.autoplay.stop(); // Stop autoplay during mouse dragging
+    }
+  });
+
+  swiper2.on('transitionEnd', function () {
+    if (!isMouseDown) {
+      if (swiper2.isEnd) {
+        swiper2.slideTo(1, 0); // Scroll to the left and if you have reached the last slide, go back to the beginning
+      } else if (swiper2.isBeginning) {
+        swiper2.slideTo(swiper2.slides.length - 2, 0); // Scroll to the right and, if to the first slide, return to the end
+      }
+      swiper2.autoplay.start(); // Start autoplay when mouse drag ends
+    }
+  });
+}
 
 
 
